249d310d90b00c6a3da529813bd4bdfd1f28e191
sakozoko <olesis.ko@icloud.com>
2022-12-24 21:36:44 +0200

    upd interfaces

--- src/Application/Interfaces/Repositories/IUserRepository.cs
+++ src/Application/Interfaces/Repositories/IUserRepository.cs
@@ -4,7 +4,7 @@ namespace Application.Interfaces.Repositories;
 
 public interface IUserRepository : IGenericRepository<UserEntity>
 {
-    public Task<UserEntity> GetByEmailAsync(string email);
-    public Task<UserEntity> GetByIdWithDetailsAsync(int id);
-    public Task<IQueryable<UserEntity>> GetAllWithDetailsAsync();
+    public Task<UserEntity?> GetByEmailAsync(string email);
+    public Task<UserEntity?> GetByIdWithDetailsAsync(int id);
+    public Task<IQueryable<UserEntity?>> GetAllWithDetailsAsync();
 }
