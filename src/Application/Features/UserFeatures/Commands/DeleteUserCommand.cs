43f3d454bcea105c17a6d2e9a4c0c5a286906e3a
sakozoko <olesis.ko@icloud.com>
2022-12-28 22:07:30 +0200

    implement DeleteUserCommand

---
+++ src/Application/Features/UserFeatures/Commands/DeleteUserCommand.cs
@@ -0,0 +1,36 @@
+﻿using Application.Extension.Repository;
+using Application.Interfaces.Repositories;
+using Domain.Entities;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Features.UserFeatures.Commands;
+
+public class DeleteUserCommand : IRequest<UserEntity>
+{
+    public int Id { get; set; }
+
+    public class DeleteUserCommandValidator : AbstractValidator<DeleteUserCommand>
+    {
+        public DeleteUserCommandValidator(IUserRepository userRepository)
+        {
+            RuleFor(d => d.Id).NotEmpty()
+                .GreaterThan(0)
+                .Must(userRepository.UserIsExist)
+                .WithMessage("User must exist");
+        }
+
+        public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, UserEntity>
+        {
+            private readonly IUserRepository _userRepository;
+
+            public DeleteUserCommandHandler(IUserRepository userRepository)
+            {
+                _userRepository = userRepository;
+            }
+            public async Task<UserEntity> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+                => await _userRepository.DeleteAsync(request.Id, cancellationToken);
+            
+        }
+    }
+}
