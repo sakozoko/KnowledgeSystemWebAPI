0762ffbfd572c1afbcf2c72008ab0ab035e9cd32
sakozoko <olesis.ko@icloud.com>
2022-12-28 22:08:27 +0200

    implement UpdateUserCommand.cs

--- src/Application/Features/UserFeatures/Commands/UpdateUserCommand.cs
+++ src/Application/Features/UserFeatures/Commands/UpdateUserCommand.cs
@@ -1,14 +1,15 @@
 ﻿using Application.Interfaces.Repositories;
 using FluentValidation;
 using MediatR;
+using Application.Extension.Repository;
 
 namespace Application.Features.UserFeatures.Commands;
 
 public class UpdateUserCommand : IRequest<int>
 {
-    public int? Id { get; set; }
+    public int Id { get; set; }
     public string? UserName { get; set; }
-    public string? Name { get; set; }
+    public string? FirstName { get; set; }
     public string? Surname { get; set; }
     public string? Phone { get; set; }
     public string? Email { get; set; }
@@ -17,16 +18,37 @@ public class UpdateUserCommand : IRequest<int>
 
     public class UpdateUserCommandValidation : AbstractValidator<UpdateUserCommand>
     {
-        public UpdateUserCommandValidation()
+        public UpdateUserCommandValidation(IUserRepository userRepository, IRoleRepository roleRepository)
         {
-            RuleFor(c => c.Id).NotEmpty().GreaterThan(0);
-            RuleFor(c=>c.Email).NotEmpty().Matches("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$").MaximumLength(100);
-            RuleFor(c=>c.Password).NotEmpty().MaximumLength(100);
-            RuleFor(c=>c.UserName).NotEmpty().MaximumLength(100);
-            RuleFor(c=>c.Name).NotEmpty().MaximumLength(100);
-            RuleFor(c=>c.Surname).NotEmpty().MaximumLength(100);
-            RuleFor(c=>c.Phone).NotEmpty().Matches("^[0-9]*$").MaximumLength(20);
-            RuleFor(c=>c.Role).NotEmpty().MaximumLength(30);
+            RuleFor(c => c.Id)
+                .NotEmpty()
+                .GreaterThan(0)
+                .Must(userRepository.UserIsExist)
+                .WithMessage("User must exist");
+            RuleFor(c=>c.Email).NotEmpty()
+                .MaximumLength(320)
+                .EmailAddress()
+                .Must(userRepository.EmailIsUnique)
+                .WithMessage("Email must be unique, length less than 320");
+            RuleFor(c=>c.Password).NotEmpty()
+                .MaximumLength(128);
+            RuleFor(c=>c.UserName).NotEmpty()
+                .MaximumLength(64)
+                .Must(userRepository.UserNameIsUnique)
+                .WithMessage("Username must be unique, length less than 64");
+            RuleFor(c=>c.FirstName).NotEmpty()
+                .MaximumLength(64);
+            RuleFor(c=>c.Surname).NotEmpty()
+                .MaximumLength(64);
+            RuleFor(c=>c.Phone).NotEmpty()
+                .Matches("^[0-9]*$")
+                .MaximumLength(15)
+                .Must(userRepository.PhoneIsUnique)
+                .WithMessage("Phone must be unique, length less than 15");
+            RuleFor(c=>c.Role).NotEmpty()
+                .MaximumLength(32)
+                .Must(roleRepository.RoleIsExist)
+                .WithMessage("Role must exist, length less than 32");
         }
     }
 
@@ -43,26 +65,19 @@ public class UpdateUserCommand : IRequest<int>
 
         public async Task<int> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
         {
-            var user = await _userRepository.GetByIdWithDetailsAsync(request.Id.Value);
-            if (user is null)
-            {
-                return default;
-            }
-            var role = await _roleRepository.GetRoleByNameAsync(request.Role);
-            if (role is null)
-            {
-                return default;
-            }
-
+            var user = await _userRepository.GetByIdWithDetailsAsync(request.Id,cancellationToken);
+            var role = await _roleRepository.GetRoleByNameAsync(request.Role,cancellationToken);
+            
             user.Role = role;
             user.CreatedDate = DateTime.Now;
             user.Email = request.Email;
             user.Password = request.Password;
             user.Phone = request.Phone;
-            user.FirstName = request.Name;
+            user.FirstName = request.FirstName;
             user.Surname = request.Surname;
             user.UserName = request.UserName;
-            
+
+            await _userRepository.UpdateAsync(user, cancellationToken);
 
             return user.Id;
         }
