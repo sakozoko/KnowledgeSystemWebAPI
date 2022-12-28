56084c5595f9f06bd09122a03b76152db014282f
sakozoko <olesis.ko@icloud.com>
2022-12-28 22:08:37 +0200

    implement GetUsersQuery.cs

---
+++ src/Application/Features/UserFeatures/Queries/GetUsersQuery.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.UserFeatures.Queries;
+
+public class GetUsersQuery : IRequest<IEnumerable<UserEntity>>
+{
+    public class GetUsersQueryHandler : IRequestHandler<GetUsersQuery, IEnumerable<UserEntity>>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUsersQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IEnumerable<UserEntity>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+            => await _userRepository.GetAllWithDetailsAsync(cancellationToken);
+    }
+}
