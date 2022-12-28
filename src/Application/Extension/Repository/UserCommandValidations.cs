c0df532c4de7c4c8d20494e31816ccfdc06dbd1a
sakozoko <olesis.ko@icloud.com>
2022-12-28 22:08:05 +0200

    simple repository extensions method for validation

---
+++ src/Application/Extension/Repository/UserCommandValidations.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces.Repositories;
+
+namespace Application.Extension.Repository;
+
+public static class UserCommandValidations
+{
+    public static bool EmailIsUnique(this IUserRepository userRepository, string email)
+    {
+        return userRepository.GetByEmailAsync(email).Result is null;
+    }
+
+    public static bool UserNameIsUnique(this IUserRepository userRepository, string username)
+    {
+        return userRepository.GetByUserNameAsync(username).Result is null;
+    }
+
+    public static bool UserIsExist(this IUserRepository userRepository, int id)
+    {
+        return userRepository.GetByIdWithDetailsAsync(id).Result is not null;
+    }
+
+    public static bool PhoneIsUnique(this IUserRepository userRepository, string phone)
+        => userRepository.GetByPredicateAsync(c => c.Phone == phone).Result is null;
+
+    public static bool RoleIsExist(this IRoleRepository roleRepository ,string roleName)
+    {
+        return roleRepository.GetRoleByNameAsync(roleName).Result is not null;
+    }
+}
