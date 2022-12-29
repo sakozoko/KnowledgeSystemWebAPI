e0e1bac5f0eef0f4d03e18e5fd9d90826c9586e3
sakozoko <olesis.ko@icloud.com>
2022-12-29 22:27:40 +0200

    fixed naming of ext methods of UserCommandValidations.cs

--- src/Application/Extension/Repository/UserCommandValidations.cs
+++ src/Application/Extension/Repository/UserCommandValidations.cs
@@ -4,27 +4,27 @@ namespace Application.Extension.Repository;
 
 public static class UserCommandValidations
 {
-    public static bool EmailIsUnique(this IUserRepository userRepository, string email)
+    public static bool IsEmailUnique(this IUserRepository userRepository, string email)
     {
         return userRepository.GetByEmailAsync(email).Result is null;
     }
 
-    public static bool UserNameIsUnique(this IUserRepository userRepository, string username)
+    public static bool IsUserNameUnique(this IUserRepository userRepository, string username)
     {
         return userRepository.GetByUserNameAsync(username).Result is null;
     }
 
-    public static bool UserIsExist(this IUserRepository userRepository, int id)
+    public static bool IsUserExist(this IUserRepository userRepository, int id)
     {
         return userRepository.GetByIdWithDetailsAsync(id).Result is not null;
     }
 
-    public static bool PhoneIsUnique(this IUserRepository userRepository, string phone)
+    public static bool IsPhoneUnique(this IUserRepository userRepository, string phone)
     {
         return userRepository.GetByPredicateAsync(c => c.Phone == phone).Result is null;
     }
 
-    public static bool RoleIsExist(this IRoleRepository roleRepository, string roleName)
+    public static bool IsRoleExist(this IRoleRepository roleRepository, string roleName)
     {
         return roleRepository.GetRoleByNameAsync(roleName).Result is not null;
     }
