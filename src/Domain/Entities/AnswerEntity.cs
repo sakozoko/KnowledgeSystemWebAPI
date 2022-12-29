306448141ddb7a93e911ee7b1afd8b973392989e
sakozoko <olesis.ko@icloud.com>
2022-12-29 19:41:15 +0200

    updated entities

--- src/Domain/Entities/AnswerEntity.cs
+++ src/Domain/Entities/AnswerEntity.cs
@@ -2,7 +2,7 @@
 
 public class AnswerEntity : BaseEntity
 {
-    public string? Text { get; set; }
-    public bool? IsCorrect { get; set; }
+    public string Text { get; set; }
+    public bool IsCorrect { get; set; }
     public QuestionEntity? Question { get; set; }
 }
--- src/Domain/Entities/QuestionEntity.cs
+++ src/Domain/Entities/QuestionEntity.cs
@@ -4,6 +4,6 @@ public class QuestionEntity : BaseEntity
 {
     public string? Text { get; set; }
     public TestEntity? Test { get; set; }
-    public double? Mark { get; set; }
+    public decimal? Mark { get; set; }
     public ICollection<AnswerEntity>? Answers { get; set; }
 }
